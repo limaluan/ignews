53dd5d4e683e22d9d7533126d7901d2e4a1ad894
limaluan <limaluan.dev@gmail.com>
2021-10-13 16:04:45 -0300

    Componente SignInButton

---
+++ src/Components/SignInButton/index.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+    
+    return isUserLoggedIn ? (
+        <button
+        type="button"
+        className={styles.SignInButton}>
+            <FaGithub color='#04d361'/>
+            Luan o Zika
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+        type="button"
+        className={styles.SignInButton}>
+            <FaGithub color='#eba417'/>
+            Sign in with Github
+        </button>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '../../styles/global.scss';
-import { Header } from '../Components/Header';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
