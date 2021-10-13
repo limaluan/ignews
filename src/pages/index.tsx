f8b4a6bdfc33be81de0eda830c868a1a49236a24
limaluan <limaluan.dev@gmail.com>
2021-10-13 17:13:35 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
+import styles from './home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
     return (
         <>
             <Head>
-                <title>Início | ig.news</title>
+                <title>Home | ig.news</title>
             </Head>
-            <h1>Hello World</h1>
+            <main className={styles.homeContainer}>
+                <section className={styles.hero}>
+                    <span>👏 Hey, welcome</span>
+                    <h1>News about the <span>React</span> world.</h1>
+                    <p>
+                        Get acess to all publications <br />
+                        <span>for $9.90 month</span>
+                    </p>
+                </section>
+
+                <img src="/images/avatar.svg" alt="Girl coding" />
+            </main>
         </>
     )
 }
