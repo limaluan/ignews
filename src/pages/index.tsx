c52fdcfcf10a2a5e6c72afc6bf689636287941fa
limaluan <limaluan.dev@gmail.com>
2022-07-20 21:30:43 -0300

    adds: application structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "../../node_modules/next/head";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ínicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello World.
+      </h1>
+    </>
+  )
+}
