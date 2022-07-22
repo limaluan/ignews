30a46c43aa8c6a4d2634550aaa05a5c8c4e59def
limaluan <limaluan.dev@gmail.com>
2022-07-22 14:26:54 -0300

    adds: components and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import Head from "../../node_modules/next/head";
+import { SubscribeButton } from "../components/SubscribeButton/index";
 
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ínicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello World.
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
